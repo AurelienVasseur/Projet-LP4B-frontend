634a489c12b7af75a61b4678cde1a73e842d9aff
AurelienVasseur <aurelienvasseur60@gmail.com>
2019-11-15 11:22:08 +0100

    Update fonction increment

--- functions/increment.js
+++ functions/increment.js
@@ -1,8 +1,20 @@
+import fetch from "node-fetch";
+
+const API_ENDPOINT = "https://lp4asgadot.herokuapp.com/counters/";
+
 exports.handler = async (event, context) => {
     const id = event.queryStringParameters.id;
 
-    return {
-        statusCode: 200,
-        body: `Compteur : ${id}`
-    };
+    const route = API_ENDPOINT + id;
+
+    return fetch(route, { 
+        headers: { "Accept": "application/json" },
+        method: "PATCH"
+    })
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data.joke
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
 };
